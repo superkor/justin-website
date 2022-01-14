e3a0aa2b4ad5b663dbce6e7ce6c2b8ba03e59202
superkor <justinchow03@hotmail.com>
2022-01-14 10:03:44 -0500

    fixed opacity infinite countup in event listener

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //This script runs when the page finishes loading. This will cause the page to fade in as a transistion.
 let interval;
 let opacity = 0 ;
-let interval_two;
 function fadeInStart(){
     interval = setInterval(function() {
             opacity += 0.01;
@@ -18,26 +17,29 @@ function stopTransitionOnLoadStart(){
         document.getElementById("titleWrapper").style.display = "block";
         document.getElementById("home").style.display = "block";
         homeFadeIn();
+        start.style.opacity = 0;
         opacity = 0;
     }    
 }
 
 function homeFadeIn(){
-    console.log("test");
-    interval_two = setInterval(function() {
+    interval = setInterval(function() {
             opacity += 0.01;
-            document.getElementsByTagName("body")[0].style.opacity = opacity;
+            start.style.opacity = opacity;
             //console.log(info.opacity);
         }, 5);
 }
 
 function stopTransitionHome(){
-    if (opacity > 1){
-            clearInterval(interval_two);
-            interval_two = null;
-            opacity = 0;
-        }
+    if(document.getElementById("titleWrapper").style.display == "block"){
+        if (opacity > 1){
+                clearInterval(interval);
+                interval = null;
+                opacity = 0;
+                start.style.opacity = 1;
+            }
+    }
 }
 
 document.getElementById("clearButton").addEventListener("click", stopTransitionOnLoadStart);
-document.getElementsByTagName("body")[0].addEventListener("onmousemove", stopTransitionHome);
+document.getElementById("start").addEventListener("mousemove", stopTransitionHome);
