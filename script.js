1ed5a97c7a37c3a73c479caa789fac3cbc1081d2
superkor <justinchow03@hotmail.com>
2022-01-14 09:40:45 -0500

    Added fade in after button click separated js and css files

---
+++ script.js
@@ -0,0 +1,44 @@
+//This script runs when the page finishes loading. This will cause the page to fade in as a transistion.
+let interval;
+let opacity = 0 ;
+let interval_two;
+function fadeInStart(){
+    interval = setInterval(function() {
+            opacity += 0.01;
+            start.style.opacity = opacity;
+            //console.log(info.opacity);
+        }, 5);
+}
+
+function stopTransitionOnLoadStart(){
+    if (opacity > 1){
+        clearInterval(interval);
+        interval = null;
+        document.getElementById("Title").style.display = "none";
+        document.getElementById("titleWrapper").style.display = "block";
+        document.getElementById("home").style.display = "block";
+        homeFadeIn();
+        opacity = 0;
+    }    
+}
+
+function homeFadeIn(){
+    console.log("test");
+    interval_two = setInterval(function() {
+            opacity += 0.01;
+            document.getElementsByTagName("body")[0].style.opacity = opacity;
+            //console.log(info.opacity);
+        }, 5);
+}
+
+function stopTransitionHome(){
+    if (opacity > 1){
+            clearInterval(interval_two);
+            interval_two = null;
+            homeFadeIn();
+            opacity = 0;
+        }
+}
+
+document.getElementById("clearButton").addEventListener("click", stopTransitionOnLoadStart);
+document.getElementsByTagName("body")[0].addEventListener("onmousemove", stopTransitionHome);
