af61e87f54297e720e94521829851dcc27d010c2
superkor <justinchow03@hotmail.com>
2022-01-29 21:29:20 -0500

    fixed id names

--- Home/homeScript.js
+++ Home/homeScript.js
@@ -113,7 +113,7 @@ function getRepoInfo(){
 
         $.each(data, function(i, status){
             //Begin Card
-            statusHTML+='<div id="Project'+i+'"class="projectCards" style="text-align: center;">';
+            statusHTML+='<div id="Project'+i+'" class="projectCards" style="text-align: center;">';
             //Header for card
             statusHTML+='<h3>'+'<a href='+status.html_url+' style="text-decoration: none; color: #FFD700; font-weight: bold;">'+status.name+'</a></h3>';
             //Description
@@ -149,7 +149,7 @@ function getExperienceInfo(){
             var output = '';
 
             $.each(jobs, function(i, status){
-                output += '<div id="Experience"'+i+' class="cards" style="text-align: center;">';
+                output += '<div id="Experience'+i+'" class="cards" style="text-align: center;">';
                 output += '<h2><a href="'+status.companyLink+'" style="text-decoration: none; color: #FFD700; font-weight: bold;">'+status.jobTitle+'</a></h2>';
                 output += '<h3>'+status.companyName+'</h3>';
                 output += '<h4 style="font-weight: normal;">'+status.location+'</h4>';
@@ -183,7 +183,7 @@ function getEducationInfo(){
         var output = '';
 
         $.each(jobs, function(i, status){
-            output += '<div id="Education"'+i+' class="cards" style="text-align: center;">';
+            output += '<div id="educationCard'+i+'" class="cards" style="text-align: center;">';
             output += '<h2><a href="'+status.link+'" style="text-decoration: none; color: #FFD700; font-weight: bold;">'+status.degree+'</a></h2>';
             output += '<h3>'+status.school+'</h3>';
             output += '<h4 style="font-weight: normal;">'+status.location+'</h4>';
